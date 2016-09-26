316ca4b8192a12c6e197ad05092176cde3a06078
Eldar <Eldar@ELDAR-LAPTOP>
2016-09-26 15:53:54 +0300

    Add project files.

---
+++ ExternalLogins_MVC5/Controllers/HomeController.cs
@@ -0,0 +1,289 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Facebook;
+using Microsoft.AspNet.Identity;
+using Newtonsoft.Json;
+
+namespace ExternalLogins_MVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ExternalLoginFailure()
+        {
+            return View();
+        }
+        public ActionResult ExternalLoginConfirmation()
+        {
+            return View();
+        }
+        public ActionResult GitHub()
+        {
+            return View();
+        }
+        private IAuthenticationManager AuthenticationManager
+        {
+            get { return HttpContext.GetOwinContext().Authentication; }
+        }
+
+
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public void ExternalLogin(string provider, string returnUrl = null)
+        {
+            var properties = new AuthenticationProperties { RedirectUri = Url.Action("ExternalLoginCallback", "Home", new { ReturnUrl = returnUrl }) };
+            ControllerContext.HttpContext.GetOwinContext().Authentication.Challenge(properties, provider);
+
+        }
+
+        private string m_url;
+
+        [JsonProperty("url")]
+        public string ImageUrl
+        {
+            get { return m_url; }
+            set { m_url = value; }
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult> ExternalLoginCallback(string returnUrl = null)
+        {
+            ExternalLoginConfirmationViewModel model = new ExternalLoginConfirmationViewModel();
+            var info = await AuthenticationManager.GetExternalLoginInfoAsync();
+
+            if (info == null) { return RedirectToAction("Index", "Home"); }
+            ViewBag.LoginProvider = info.Login.LoginProvider;
+
+            if (info.Login.LoginProvider == "Facebook")
+            {
+                var identity = AuthenticationManager.GetExternalIdentity(DefaultAuthenticationTypes.ExternalCookie);
+                var accessToken = identity.FindFirstValue("FacebookAccessToken");
+                var fb = new FacebookClient(accessToken);
+                dynamic email = fb.Get("me?fields=email");
+                dynamic birthday = fb.Get("me?fields=birthday");
+                dynamic name = fb.Get("me?fields=name");
+                dynamic first_name = fb.Get("me?fields=first_name");
+                dynamic last_name = fb.Get("me?fields=last_name");
+                dynamic link = fb.Get("me?fields=link");
+                dynamic gender = fb.Get("me?fields=gender");
+                dynamic locale = fb.Get("me?fields=locale");
+                //You can find other fields at https://developers.facebook.com/docs/graph-api/reference/user
+
+                model = AddToModel(name.name, null, first_name.first_name, last_name.last_name, email.email, birthday.birthday, link.link, gender.gender, locale.locale, null, null, null);
+            }
+            else if (info.Login.LoginProvider == "Google")
+            {
+                string emailaddress = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
+                string name = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name").Value;
+                string givenname = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname").Value;
+                string surname = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname").Value;
+                string url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:google:url").Value;
+                string profile = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:google:profile").Value;
+                //Profile and Url returns same value.
+                string image = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:google:image").Value;
+                var imageUrl = JsonConvert.DeserializeObject<HomeController>(image).ImageUrl;
+                model = AddToModel(name, null, givenname, surname, emailaddress, null, url, null, null, null, null, imageUrl);
+            }
+            else if (info.Login.LoginProvider == "Microsoft")
+            {
+                string birth_day = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:microsoft:birth_day").Value;
+                string birth_month = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:microsoft:birth_month").Value;
+                string birth_year = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:microsoft:birth_year").Value;
+                string birthday = birth_month + "/" + birth_day + "/" + birth_year;
+                string email = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email).Value;
+                string name = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:microsoft:name").Value;
+                string first_name = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:microsoft:first_name").Value;
+                string last_name = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:microsoft:last_name").Value;
+                string link = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:microsoft:link").Value;
+                string gender = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:microsoft:gender").Value;
+                string locale = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:microsoft:locale").Value;
+
+                model = AddToModel(name, null, first_name, last_name, email, birthday, link, gender, locale, null, null, null);
+            }
+            else if (info.Login.LoginProvider == "LinkedIn")
+            {
+                string id = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:linkedin:id").Value;
+                string imageUrl = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:linkedin:pictureUrl").Value;
+                string name = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:linkedin:name").Value;
+                string formattedName = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:linkedin:formattedName").Value;
+                string firstName = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:linkedin:firstName").Value;
+                string lastName = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:linkedin:lastName").Value;
+                string emailAddress = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:linkedin:emailAddress").Value;
+                string headline = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:linkedin:headline").Value;
+                string url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:linkedin:url").Value;
+                string publicProfileUrl = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:linkedin:publicProfileUrl").Value;
+
+                //You can find other fields at https://developer.linkedin.com/docs/fields/basic-profile
+
+                model = AddToModel(name, formattedName, firstName, lastName, emailAddress, null, url, null, null, headline, publicProfileUrl, imageUrl);
+            }
+            else if (info.Login.LoginProvider == "Twitter")
+            {
+                string userid = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:twitter:userid").Value;
+                string screenname = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:twitter:screenname").Value;
+
+            }
+            else if (info.Login.LoginProvider == "GitHub")
+            {
+                string name = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name").Value;
+                string emailaddress = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress").Value;
+                string url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:url").Value;
+                string login = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:login").Value;
+                string id = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:id").Value;
+                string avatar_url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:avatar_url").Value;
+                string gravatar_id = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:gravatar_id").Value;
+                string html_url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:html_url").Value;
+                string followers_url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:followers_url").Value;
+                string following_url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:following_url").Value;
+                string gists_url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:gists_url").Value;
+                string starred_url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:starred_url").Value;
+                string subscriptions_url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:subscriptions_url").Value;
+                string organizations_url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:organizations_url").Value;
+                string repos_url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:repos_url").Value;
+                string events_url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:events_url").Value;
+                string received_events_url = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:received_events_url").Value;
+                string type = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:type").Value;
+                string site_admin = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:site_admin").Value;
+                string name2 = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:name").Value;
+                string company = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:company").Value;
+                string blog = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:blog").Value;
+                string location = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:location").Value;
+                string email = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:email").Value;
+                string hireable = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:hireable").Value;
+                string bio = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:bio").Value;
+                string public_repos = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:public_repos").Value;
+                string public_gists = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:public_gists").Value;
+                string followers = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:followers").Value;
+                string following = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:following").Value;
+                string created_at = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:created_at").Value;
+                string updated_at = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:updated_at").Value;
+                string private_gists = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:private_gists").Value;
+                string total_private_repos = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:total_private_repos").Value;
+                string owned_private_repos = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:owned_private_repos").Value;
+                string disk_usage = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:disk_usage").Value;
+                string collaborators = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:collaborators").Value;
+                string plan = info.ExternalIdentity.Claims.FirstOrDefault(c => c.Type == "urn:github:plan").Value;
+
+                string[] git = new string[] { name, emailaddress, url, login, id, avatar_url, gravatar_id, html_url, followers_url, following_url, gists_url, starred_url, subscriptions_url, organizations_url, repos_url, events_url, received_events_url, type, site_admin, name2, company, blog, location, email, hireable, bio, public_repos, public_gists, followers, following, created_at, updated_at, private_gists, total_private_repos, owned_private_repos, disk_usage, collaborators, plan };
+                return View("GitHub", git);
+            }
+            else
+            {
+                model.Email = null;
+                model.BirthDate = null;
+                model.FirstName = null;
+                model.LastName = null;
+            }
+            return View("ExternalLoginConfirmation", model);
+        }
+
+        public ExternalLoginConfirmationViewModel AddToModel(string name, string formattedName, string firstName, string lastName, string email, string birthday, string link, string gender, string locale, string headline, string publicProfileUrl, string imageUrl)
+        {
+            var model = new ExternalLoginConfirmationViewModel();
+            model.BirthDate = birthday;
+            model.Email = email;
+            model.FirstName = firstName;
+            model.FormattedName = formattedName;
+            model.Gender = gender;
+            model.Headline = headline;
+            model.LastName = lastName;
+            model.Link = link;
+            model.Locale = locale;
+            model.Name = name;
+            model.PublicProfileUrl = publicProfileUrl;
+            model.ImageUrl = imageUrl;
+            return model;
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExternalLoginConfirmation(ExternalLoginConfirmationViewModel model, string returnUrl = null)
+        {
+            //ToDo
+            return View("Index");
+        }
+
+
+        public IEnumerable<SelectListItem> GetCountries()
+        {
+            List<SelectListItem> countryNames = new List<SelectListItem>();
+            countryNames.Add(new SelectListItem { Text = "AAA", Value = "AAA" });
+            countryNames.Add(new SelectListItem { Text = "BBB", Value = "BBB" });
+            countryNames.Add(new SelectListItem { Text = "CCC", Value = "CCC" });
+            countryNames.Add(new SelectListItem { Text = "DDD", Value = "DDD" });
+            return countryNames;
+        }
+
+    }
+
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Name (All)")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "FormattedName (LinkedIn)")]
+        public string FormattedName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name (All)")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name (All)")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email (All)")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "BirthDate (Facebook, Microsoft)")]
+        public string BirthDate { get; set; }
+
+        [Required]
+        [Display(Name = "Link (All)")]
+        public string Link { get; set; }
+
+
+        [Required]
+        [Display(Name = "Gender (Facebook, Microsoft)")]
+        public string Gender { get; set; }
+
+
+        [Required]
+        [Display(Name = "Locale (Facebook, Microsoft)")]
+        public string Locale { get; set; }
+
+
+        [Required]
+        [Display(Name = "Headline (LinkedIn)")]
+        public string Headline { get; set; }
+
+
+        [Required]
+        [Display(Name = "PublicProfileUrl (LinkedIn)")]
+        public string PublicProfileUrl { get; set; }
+
+        [Required]
+        [Display(Name = "ImageUrl (Google, LinkedIn)")]
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ ExternalLogins_MVC5/Startup.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Facebook;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.MicrosoftAccount;
+using Microsoft.Owin.Security.Twitter;
+using Owin;
+using Owin.Security.Providers.GitHub;
+using Owin.Security.Providers.LinkedIn;
+using System.Configuration;
+
+[assembly: OwinStartupAttribute(typeof(ExternalLogins_MVC5.Startup))]
+namespace ExternalLogins_MVC5
+{
+    public class Startup
+    {
+        public Startup()
+        {
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        //This method is inside App_Start/Startup.Auth.cs
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            #region Microsoft
+
+            var microsoftAuthenticationOptions = new MicrosoftAccountAuthenticationOptions()
+            {
+                ClientId = ConfigurationManager.AppSettings["MsI"],
+                ClientSecret = ConfigurationManager.AppSettings["MsS"],
+                Provider = new MicrosoftAccountAuthenticationProvider()
+                {
+                    OnAuthenticated = async context =>
+                    {
+                        context.Identity.AddClaim(new System.Security.Claims.Claim("MicrosoftAccessToken", context.AccessToken));
+                        foreach (var claim in context.User)
+                        {
+                            var claimType = string.Format("urn:microsoft:{0}", claim.Key);
+                            string claimValue = claim.Value.ToString();
+                            if (!context.Identity.HasClaim(claimType, claimValue))
+                                context.Identity.AddClaim(new System.Security.Claims.Claim(claimType, claimValue, "XmlSchemaString", "Microsoft"));
+                        }
+                    }
+                }
+            };
+            microsoftAuthenticationOptions.Scope.Add("wl.basic");
+            microsoftAuthenticationOptions.Scope.Add("wl.emails");
+            microsoftAuthenticationOptions.Scope.Add("wl.birthday");
+            app.UseMicrosoftAccountAuthentication(microsoftAuthenticationOptions);
+
+            #endregion Microsoft
+
+            #region Twitter
+            var twitterAuthenticationOptions = new TwitterAuthenticationOptions()
+            {
+                ConsumerKey = ConfigurationManager.AppSettings["TwtI"],
+                ConsumerSecret = ConfigurationManager.AppSettings["TwtS"],
+                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(
+                new[]
+                {
+                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                "4eb6d578499b1ccf5f581ead56be3d9b6744a5e5", // VeriSign Class 3 Primary CA - G5
+                "5168FF90AF0207753CCCD9656462A212B859723B", // DigiCert SHA2 High Assurance Server C‎A 
+                "B13EC36903F8BF4701D498261A0802EF63642BC3" // DigiCert High Assurance EV Root CA
+                }),
+                Provider = new TwitterAuthenticationProvider()
+                {
+                    OnAuthenticated = async context =>
+                    {
+                        context.Identity.AddClaim(new System.Security.Claims.Claim("TwitterAccessToken", context.AccessToken));
+                        foreach (var claim in context.UserId)
+                        {
+                            var claimType = string.Format("urn:twitter:{0}", claim);
+                            string claimValue = claim.ToString();
+                            if (!context.Identity.HasClaim(claimType, claimValue))
+                                context.Identity.AddClaim(new System.Security.Claims.Claim(claimType, claimValue, "XmlSchemaString", "Twitter"));
+                        }
+                    }
+                }
+            };
+            app.UseTwitterAuthentication(twitterAuthenticationOptions);
+
+            #endregion Twitter
+
+            #region Facebook
+
+            var facebookAuthenticationOptions = new FacebookAuthenticationOptions()
+            {
+                AppId = ConfigurationManager.AppSettings["FaceI"],
+                AppSecret = ConfigurationManager.AppSettings["FaceS"],
+                Provider = new FacebookAuthenticationProvider()
+                {
+                    OnAuthenticated = async context =>
+                    {
+                        context.Identity.AddClaim(new System.Security.Claims.Claim("FacebookAccessToken", context.AccessToken));
+                        foreach (var claim in context.User)
+                        {
+                            var claimType = string.Format("urn:facebook:{0}", claim.Key);
+                            string claimValue = claim.Value.ToString();
+                            if (!context.Identity.HasClaim(claimType, claimValue))
+                                context.Identity.AddClaim(new System.Security.Claims.Claim(claimType, claimValue, "XmlSchemaString", "Facebook"));
+                        }
+                    }
+                }
+            };
+            facebookAuthenticationOptions.Scope.Add("public_profile");
+            facebookAuthenticationOptions.Scope.Add("email");
+            facebookAuthenticationOptions.Scope.Add("user_birthday");
+            app.UseFacebookAuthentication(facebookAuthenticationOptions);
+
+            #endregion Facebook
+
+            #region Google
+
+            var googleAuthenticationOptions = new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = ConfigurationManager.AppSettings["GglI"],
+                ClientSecret = ConfigurationManager.AppSettings["GglS"],
+                Provider = new GoogleOAuth2AuthenticationProvider()
+                {
+                    OnAuthenticated = async context =>
+                    {
+                        context.Identity.AddClaim(new System.Security.Claims.Claim("GoogleAccessToken", context.AccessToken));
+                        foreach (var claim in context.User)
+                        {
+                            var claimType = string.Format("urn:google:{0}", claim.Key);
+                            string claimValue = claim.Value.ToString();
+                            if (!context.Identity.HasClaim(claimType, claimValue))
+                                context.Identity.AddClaim(new System.Security.Claims.Claim(claimType, claimValue, "XmlSchemaString", "Google"));
+                        }
+                    }
+                }
+            };
+            googleAuthenticationOptions.Scope.Add("https://www.googleapis.com/auth/plus.login email");
+            app.UseGoogleAuthentication(googleAuthenticationOptions);
+
+            #endregion Google
+
+            #region LinkedIn
+
+            var linkedinAuthenticationOptions = new LinkedInAuthenticationOptions()
+            {
+                ClientId = ConfigurationManager.AppSettings["LinkI"],
+                ClientSecret = ConfigurationManager.AppSettings["LinkS"],
+                Provider = new LinkedInAuthenticationProvider()
+                {
+                    OnAuthenticated = async context =>
+                    {
+                        context.Identity.AddClaim(new System.Security.Claims.Claim("LinkedInAccessToken", context.AccessToken));
+                        foreach (var claim in context.User)
+                        {
+                            var claimType = string.Format("urn:linkedin:{0}", claim.Key);
+                            string claimValue = claim.Value.ToString();
+                            if (!context.Identity.HasClaim(claimType, claimValue))
+                                context.Identity.AddClaim(new System.Security.Claims.Claim(claimType, claimValue, "XmlSchemaString", "LinkedIn"));
+                        }
+                    }
+                }
+            };
+            app.UseLinkedInAuthentication(linkedinAuthenticationOptions);
+
+            #endregion LinkedIn
+
+            #region GitHub
+
+            var gitAuthanticationOptions = new GitHubAuthenticationOptions()
+            {
+                ClientId = ConfigurationManager.AppSettings["GitI"],
+                ClientSecret = ConfigurationManager.AppSettings["GitS"],
+                Provider = new GitHubAuthenticationProvider()
+                {
+                    OnAuthenticated = async context =>
+                    {
+                        context.Identity.AddClaim(new System.Security.Claims.Claim("GitHubAccessToken", context.AccessToken));
+                        foreach (var claim in context.User)
+                        {
+                            var claimType = string.Format("urn:github:{0}", claim.Key);
+                            string claimValue = claim.Value.ToString();
+                            if (!context.Identity.HasClaim(claimType, claimValue))
+                                context.Identity.AddClaim(new System.Security.Claims.Claim(claimType, claimValue, "XmlSchemaString", "GitHub"));
+                        }
+                    }
+                }
+            };
+            app.UseGitHubAuthentication(gitAuthanticationOptions);
+
+            #endregion GitHub
+
+        }
+
+    }
+}
